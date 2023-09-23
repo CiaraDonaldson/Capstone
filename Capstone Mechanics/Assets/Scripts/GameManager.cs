622b31209145c87388759d03463fe4f56a4a360a
CiaraDonaldson <94332296+CiaraDonaldson@users.noreply.github.com>
2023-09-23 17:37:16 -0400

    Added orb count to screen

--- Capstone Mechanics/Assets/Scripts/GameManager.cs
+++ Capstone Mechanics/Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI fText;
+    public TextMeshProUGUI hText;
+    public TextMeshProUGUI fText2;
+    public TextMeshProUGUI hText2;
     public int hOrbs = 0;
     public int fOrbs = 0;
     // Start is called before the first frame update
@@ -15,7 +20,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fText.text = "Fox: " + fOrbs;
+        hText.text = "Hawk: " + hOrbs;
+        fText2.text = "Fox: " + fOrbs;
+        hText2.text = "Hawk: " + hOrbs;
+
     }
 
     public void addHOrb()
