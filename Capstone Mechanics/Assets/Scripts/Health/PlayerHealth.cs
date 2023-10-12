c60c22512b101587c5e8ae47d30b07043ef623db
CiaraDonaldson <94332296+CiaraDonaldson@users.noreply.github.com>
2023-10-12 01:49:15 -0400

    Health Done
    Just add more mays to lose health/ change how much health gets lost.

---
+++ Capstone Mechanics/Assets/Scripts/Health/PlayerHealth.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float Health, MaxHealth;
+    public GameObject Fox;
+    public bool hasBeenMet = false;
+
+    [SerializeField]
+    private HealthBarUI healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.SetMaxHealth(MaxHealth);   
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasBeenMet)
+        {
+            if (getFVelocity())
+            {
+                SetHealth(-10f);
+                hasBeenMet = true;
+                
+            }
+        }
+
+        if (Fox.GetComponent<Rigidbody2D>().velocity.y > -5)
+        {
+            hasBeenMet = false;
+        }
+
+        if (Health <= 0)
+        {
+            ReloadActiveScene();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SetHealth(20f);
+        }
+    }
+    public void SetHealth(float healthChange)
+    {
+        Health += healthChange;
+        Health = Mathf.Clamp(Health, 0, MaxHealth);
+
+        healthBar.SetHealth(Health);
+    }
+    private void ReloadActiveScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    public bool getFVelocity()
+    {
+        if (Fox.GetComponent<Rigidbody2D>().velocity.y < -12)
+        {
+            return true;
+        }
+        return false;
+    }
+}
