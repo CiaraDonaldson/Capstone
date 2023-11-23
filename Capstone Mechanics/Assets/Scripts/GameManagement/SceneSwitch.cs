072f0b96682ab3be2179d2345c9a4a7d46008c40
CiaraDonaldson <94332296+CiaraDonaldson@users.noreply.github.com>
2023-11-22 23:43:22 -0500

    Start of map system and leaves!

---
+++ Capstone Mechanics/Assets/Scripts/GameManagement/SceneSwitch.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class SceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Lvl1()
+    {
+        SceneManager.LoadScene("Lvl1");
+    }
+    public void Lvl2()
+    {
+        SceneManager.LoadScene("Lvl2");
+    }
+    public void Lvl3()
+    {
+        SceneManager.LoadScene("Lvl3");
+    }
+    public void Lvl14()
+    {
+        SceneManager.LoadScene("Lvl14");
+    }
+    public void Lvl15()
+    {
+        SceneManager.LoadScene("Lvl15");
+    }
+    public void Lvl16()
+    {
+        SceneManager.LoadScene("Lvl16");
+    }
+    public void Lvl17()
+    {
+        SceneManager.LoadScene("Lvl17");
+    }
+    public void Lvl18()
+    {
+        SceneManager.LoadScene("Lvl18");
+    }
+    public void Lvl19()
+    {
+        SceneManager.LoadScene("Lvl19");
+    }
+    public void Lvl110()
+    {
+        SceneManager.LoadScene("Lvl110");
+    }
+}
