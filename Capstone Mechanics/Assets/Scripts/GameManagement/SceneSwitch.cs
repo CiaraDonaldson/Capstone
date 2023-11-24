3e893238199bee9ee720e27effcf5609c3659897
CiaraDonaldson <94332296+CiaraDonaldson@users.noreply.github.com>
2023-11-23 23:41:36 -0500

    Pause Menu and some map setup

--- Capstone Mechanics/Assets/Scripts/GameManagement/SceneSwitch.cs
+++ Capstone Mechanics/Assets/Scripts/GameManagement/SceneSwitch.cs
@@ -20,27 +20,27 @@ public class SceneSwitch : MonoBehaviour
     {
         SceneManager.LoadScene("Lvl3");
     }
-    public void Lvl14()
+    public void Lvl4()
     {
         SceneManager.LoadScene("Lvl14");
     }
-    public void Lvl15()
+    public void Lvl5()
     {
         SceneManager.LoadScene("Lvl15");
     }
-    public void Lvl16()
+    public void Lvl6()
     {
         SceneManager.LoadScene("Lvl16");
     }
-    public void Lvl17()
+    public void Lvl7()
     {
         SceneManager.LoadScene("Lvl17");
     }
-    public void Lvl18()
+    public void Lvl8()
     {
         SceneManager.LoadScene("Lvl18");
     }
-    public void Lvl19()
+    public void Lvl9()
     {
         SceneManager.LoadScene("Lvl19");
     }
