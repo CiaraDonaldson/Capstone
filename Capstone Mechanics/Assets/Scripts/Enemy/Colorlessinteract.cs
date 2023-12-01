c7fd3b69403471ce404907c9499ee677a5fe48be
CiaraDonaldson <94332296+CiaraDonaldson@users.noreply.github.com>
2023-11-30 22:24:21 -0500

    fixed editor error and! Audio additions

--- Capstone Mechanics/Assets/Scripts/Enemy/Colorlessinteract.cs
+++ Capstone Mechanics/Assets/Scripts/Enemy/Colorlessinteract.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 public class Colorlessinteract : MonoBehaviour
 {
     public string popUp;
     public int fOrbCount = 3;
     public int hOrbCount = 2;
     public bool pass = false;
+    public GameObject popUpBox;
+    public Animator animator;
+    public TMP_Text popUpText;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,20 +26,14 @@ public class Colorlessinteract : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "Hawk")
+        if (other.gameObject.name == "Hawk" | other.gameObject.name == "Fox")
         {
-            PopUpSystem pop = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PopUpSystem>();
-            pop.PopUp(popUp);
+            PopUp(popUp);
         }
-        else if (other.gameObject.name == "Fox")
-        {
-            PopUpSystem pop = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PopUpSystem>();
-            pop.PopUp(popUp);
-        }
+     
         if (GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().fOrbs == fOrbCount && GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().hOrbs == hOrbCount)
         {
-            PopUpSystem pop = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PopUpSystem>();
-            pop.PopUp("You Did It! Thank you!");
+            PopUp("You Did It! Thank you!");
             Color lerpedColor = Color.Lerp(Color.white, Color.magenta, 20f);
             this.GetComponent<SpriteRenderer>().color = lerpedColor;
             pass = true;
@@ -50,8 +49,19 @@ public class Colorlessinteract : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            PopUpSystem pop = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PopUpSystem>();
-            pop.PopDown();
+            PopDown();
         }
     }
- }
+
+    public void PopUp(string text)
+    {
+        popUpBox.SetActive(true);
+        popUpText.text = text;
+        animator.Play("pop");
+    }
+    public void PopDown()
+    {
+        animator.Play("close");
+        //popUpBox.SetActive(false);
+    }
+}
